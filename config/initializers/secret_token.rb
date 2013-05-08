fd616e4c7eb95bebf734e43d97189bab4b882ab4
Meghna Dey <dey@bipac.org>
2013-05-08 10:42:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5904c61a076162721fb662716ff8ce184d28d566ceb7e0f66b169d2f5b1fe3833833050fa22b6afca5709ce2cf1b5e28b7e0210477aafc38045fbf98c8e2d55a'
